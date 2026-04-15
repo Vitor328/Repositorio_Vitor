1d17a7e6c7086910e61ce6ff87069e5829d22469
Vitor328 <vitor.aquino@academico.ifpb.edu.br>
2026-04-15 10:56:22 -0300

    Add files via upload

--- teste.py
+++ teste.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.image("teste.png")
-st.write("Site Dinaldo Jorge")
-st.link_button("Acessar", "https://www.dinaldo.com.br")
+st.image("WhatsApp Image 2026-04-15 at 10.49.43.Jpge")
+st.write("Site Vitor Aquino")
+st.link_button("Acessar", "https://sites.google.com/academico.ifpb.edu.br/vitor/in%C3%ADcio")
